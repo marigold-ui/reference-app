c404b3dcfe04982991e4b5d3d33db11631ac46af
aromko <aromkoapps@gmail.com>
2024-04-11 12:02:08 +0200

    DST-424: add codehike plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import {remarkCodeHike} from '@code-hike/mdx';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-});
+// @ts-ignore
+export default defineConfig(async () => {
+  const mdx = await import("@mdx-js/rollup")
+  return {
+    optimizeDeps: {
+      include: ["react/jsx-runtime"],
+    },
+    plugins: [
+      {enforce: 'pre',  ...mdx.default({
+          remarkPlugins: [[remarkCodeHike, { lineNumbers: true, showCopyButton: true,}]],
+        }),},
+      react(),
+    ],
+  }
+})
