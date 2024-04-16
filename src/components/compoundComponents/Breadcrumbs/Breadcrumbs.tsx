fb9b39bb4b6eb44f28a8df5fbc99da348a14b946
aromko <aromkoapps@gmail.com>
2024-04-16 14:10:50 +0200

    DST-411: add breadcrumbs example

---
+++ src/components/compoundComponents/Breadcrumbs/Breadcrumbs.tsx
@@ -0,0 +1,35 @@
+import { ReactNode } from 'react';
+
+function Breadcrumbs({ children }: { children: ReactNode }) {
+  return (
+    <nav aria-label="Breadcrumbs">
+      <ol className="m-0 list-none p-0">{children}</ol>
+    </nav>
+  );
+}
+
+function Crumb({
+  href,
+  isCurrent = false,
+  children,
+}: {
+  href: string;
+  isCurrent?: boolean;
+  children: ReactNode;
+}) {
+  return (
+    <li className="mr-3 inline before:content-['/'] before:first-of-type:content-['']">
+      <a
+        href={href}
+        aria-current={isCurrent && 'page'}
+        className="text-decoration-none hover:text-decoration:revert ml-3 hover:underline"
+      >
+        {children}
+      </a>
+    </li>
+  );
+}
+
+Breadcrumbs.Crumb = Crumb;
+
+export default Breadcrumbs;
