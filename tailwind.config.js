eb16129bf73b19a97da13e70e5080a540481be37
sarahgm <38324334+sarahgm@users.noreply.github.com>
2024-04-23 14:48:06 +0200

    fix: warning from tailwind.config (#22)
    remove warning from tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 import { preset } from '@marigold/theme-core/preset';
 
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [...preset.content, './src/**/*.{js,ts,jsx,tsx}'],
   presets: [preset],
 };
