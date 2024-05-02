db1b141ebae2034d2ffee21e968b87fd6754e801
aromko <aromkoapps@gmail.com>
2024-05-02 09:02:14 +0200

    DST-411: adjust tab example

--- src/components/compoundComponents/Tabs/TabsExample.tsx
+++ src/components/compoundComponents/Tabs/TabsExample.tsx
@@ -4,13 +4,13 @@ function TabsExample() {
   return (
     <Tabs>
       <div className="flex">
-        <Tabs.Tab index={0}>Tab 1</Tabs.Tab>
-        <Tabs.Tab index={1}>Tab 2</Tabs.Tab>
-        <Tabs.Tab index={2}>Tab 3</Tabs.Tab>
+        <Tabs.Tab index={0}>Mouse Settings</Tabs.Tab>
+        <Tabs.Tab index={1}>Keyboard Settings</Tabs.Tab>
+        <Tabs.Tab index={2}>Gamepad Settings</Tabs.Tab>
       </div>
-      <Tabs.TabPanel index={0}>Tabpanel 1</Tabs.TabPanel>
-      <Tabs.TabPanel index={1}>Tabpanel 2</Tabs.TabPanel>
-      <Tabs.TabPanel index={2}>Tabpanel 3</Tabs.TabPanel>
+      <Tabs.TabPanel index={0}>Adjust the sensitivity, acceleration, and button assignments for your mouse.</Tabs.TabPanel>
+      <Tabs.TabPanel index={1}>Customize the key bindings and input behavior for your keyboard.</Tabs.TabPanel>
+      <Tabs.TabPanel index={2}>Configure the controls, dead zones, and vibration settings for your gamepad.</Tabs.TabPanel>
     </Tabs>
   );
 }
