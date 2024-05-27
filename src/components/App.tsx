c9438af82aa91934b28f71dc23acfb3be513a6fd
aromko <aromkoapps@gmail.com>
2024-05-27 09:29:30 +0200

    DST-448: add content to welcome page

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,13 @@
 import '@code-hike/mdx/styles';
 import '../css/custom-ch.css';
+import WelcomeContent from './index.mdx';
 
 function App() {
   return (
     <article className="article-content">
-      WIP: Welcome to the Reference App!
+        <div>
+            <WelcomeContent />
+        </div>
     </article>
   );
 }
