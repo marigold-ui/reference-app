78fb590b0c75e38f8e63aec23992ec7cb0fef7ff
aromko <aromkoapps@gmail.com>
2024-05-27 15:06:11 +0200

    DST-450: use ghost button for demo links

--- src/components/DemoLink.tsx
+++ src/components/DemoLink.tsx
@@ -1,13 +1,24 @@
-import {Link} from '@tanstack/react-router';
+import { Link } from '@tanstack/react-router';
+import { ExternalLink } from '@marigold/icons';
+import { Button } from '@marigold/components';
 
-const DemoLink = ({ destination, text = 'View demo'}: {destination: string,  text?: string}) => {
+const DemoLink = ({
+  destination,
+  text = 'View demo',
+}: {
+  destination: string;
+  text?: string;
+}) => {
   return (
-    <div className="pb-2">
+    <div className="w-fit">
       <Link target="_blank" to={destination} search="">
-        {text}
+        <Button className="flex-1 cursor-pointer rounded-md bg-white p-4 font-black text-gray-800 hover:bg-gray-300">
+          {text}
+          <ExternalLink size={20} />
+        </Button>
       </Link>
     </div>
   );
-}
+};
 
 export default DemoLink;
