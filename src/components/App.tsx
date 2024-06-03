db4842c05e364170292060860c8010164129b6c5
Sebastian Sebald <sebastian.sebald@gmail.com>
2024-06-03 13:11:19 +0200

    fix

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,7 +5,7 @@ import WelcomeContent from './index.mdx';
 function App() {
   return (
     <article className="article-content">
-      WIP: Welcome to the Reference App!
+      <WelcomeContent />
     </article>
   );
 }
