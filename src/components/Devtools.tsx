4d33825af6f93a487211dd661f77b93601fe7ea5
Sebastian Sebald <sebastian.sebald@gmail.com>
2024-06-05 13:09:31 +0200

    fix node env

--- src/components/Devtools.tsx
+++ src/components/Devtools.tsx
@@ -1,14 +1,13 @@
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Suspense, lazy } from 'react';
 
-const TanStackRouterDevtools =
-  process.env['NODE_ENV'] === 'production'
-    ? () => null
-    : lazy(() =>
-        import('@tanstack/router-devtools').then(res => ({
-          default: res.TanStackRouterDevtools,
-        }))
-      );
+const TanStackRouterDevtools = import.meta.env.PROD
+  ? () => null
+  : lazy(() =>
+      import('@tanstack/router-devtools').then(res => ({
+        default: res.TanStackRouterDevtools,
+      }))
+    );
 
 export const Devtools = () => (
   <Suspense>
