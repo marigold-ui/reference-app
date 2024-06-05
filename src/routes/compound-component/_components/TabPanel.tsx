0e3951d1ae0864f0fa4eb2bf51cda9e9fca2b6ef
Sebastian Sebald <sebastian.sebald@gmail.com>
2024-06-05 13:25:23 +0200

    chore: update and cleanup tssconfig (#63)

--- src/routes/compound-component/_components/TabPanel.tsx
+++ src/routes/compound-component/_components/TabPanel.tsx
@@ -1,4 +1,5 @@
-import { ReactNode, useContext } from 'react';
+import { useContext } from 'react';
+import type { ReactNode } from 'react';
 import { TabsContext } from './Tabs';
 
 const TabPanel = ({
