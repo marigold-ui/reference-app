f14b36983a10f924d5743690bd5cf05502e6c847
Sebastian Sebald <sebastian.sebald@gmail.com>
2024-06-06 11:17:18 +0200

    done with prose stuff

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -5,7 +5,13 @@ import { Devtools } from '@/components/Devtools';
 const Layout = () => (
   <>
     <Header />
-    <main className="prose prose-docs mx-auto max-w-[1300px] px-[--page-padding] md:px-[--page-padding-md] xl:px-[--page-padding-xl]">
+    <main
+      className={[
+        'prose prose-docs mx-auto max-w-[1300px]',
+        'prose-code:before:content-none prose-code:after:content-none',
+        'px-[--page-padding] md:px-[--page-padding-md] xl:px-[--page-padding-xl]',
+      ].join(' ')}
+    >
       <Outlet />
     </main>
     <Devtools />
