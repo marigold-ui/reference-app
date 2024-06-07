7430eb6c87ae3a28491d85ed12df7bfa033ca344
Sebastian Sebald <sebastian.sebald@gmail.com>
2024-06-07 09:02:11 +0200

    small adjustments

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -7,14 +7,15 @@ const Page = () => {
     <>
       <Breakout
         className={[
-          "relative bg-[linear-gradient(to_bottom,_var(--tw-gradient-stops)),url('/circuit-board.svg')] bg-repeat",
-          'from-secondary-400/50 via-secondary-50 via-60% to-secondary-50',
+          'relative min-h-[calc(100vh-var(--page-header-height))]',
+          "bg-[linear-gradient(to_bottom,_var(--tw-gradient-stops)),url('/circuit-board.svg')] bg-repeat",
+          'from-secondary-400/50 via-secondary-50 via-70% to-secondary-50',
         ].join(' ')}
       >
         <div className="absolute inset-x-0 top-0 h-8 bg-gradient-to-b from-neutral-700/50 via-neutral-700/30 via-20% to-transparent" />
 
-        <div className="mx-auto min-h-[calc(100vh-var(--page-header-height))] max-w-6xl">
-          <h1 className="mb-0 text-balance text-8xl tracking-tighter lg:pt-48 lg:text-9xl">
+        <div className="mx-auto max-w-6xl pt-[8vh]">
+          <h1 className="m-0 text-balance text-8xl tracking-tighter lg:text-9xl">
             Explore Design Patterns in React
           </h1>
           <p className="mt-4 max-w-[75%] text-pretty text-lg leading-normal text-secondary-400 lg:text-2xl">
@@ -24,17 +25,17 @@ const Page = () => {
         </div>
 
         <a
-          className="absolute inset-x-0 bottom-0 grid place-items-center text-secondary-950 no-underline opacity-20 hover:opacity-40"
+          className="absolute inset-x-0 bottom-1 grid place-items-center text-secondary-950 no-underline opacity-70 hover:opacity-100"
           href="#skip-content"
         >
-          <span className="no-prose -mb-4 text-xs uppercase">Explore</span>
+          <span className="no-prose -mb-1.5 text-sm uppercase">Explore</span>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
             strokeWidth={1.5}
             stroke="currentColor"
-            className="size-20 opacity-30"
+            className="size-10 opacity-50"
           >
             <path
               strokeLinecap="round"
