223ba24a5c24526fe574863fa9207127b93c0e17
Sebastian Sebald <sebastian.sebald@gmail.com>
2024-06-11 13:18:01 +0200

    update

--- src/routes/compound-component/preview.tabs.lazy.tsx
+++
@@ -1,6 +0,0 @@
-import { createLazyFileRoute } from '@tanstack/react-router';
-import TabsExample from './_components/TabsExample';
-
-export const Route = createLazyFileRoute('/compound-component/preview/tabs')({
-  component: TabsExample,
-});
