a8f7aa60444afe138677fa5c210f84fa61a12604
ssarahgm <sarahgosmann@gmail.com>
2024-06-21 14:54:00 +0200

    fix: fix links in footer

--- src/routes/_components/Footer.tsx
+++ src/routes/_components/Footer.tsx
@@ -1,5 +1,4 @@
 import { Link } from '@tanstack/react-router';
-
 export const Footer = () => (
   <footer className="mt-48 flex w-full shrink-0 flex-col items-center gap-2 border-t px-4 py-6 sm:flex-row md:px-6">
     <p className="text-xs text-secondary-500 dark:text-secondary-400">
@@ -7,13 +6,15 @@ export const Footer = () => (
     </p>
     <nav className="flex gap-4 sm:ml-auto sm:gap-6">
       <Link
-        href="https://www.marigold-ui.io/"
+        // @ts-ignore
+        to="https://www.marigold-ui.io/"
         className="text-xs underline-offset-4 hover:underline"
       >
         Marigold Docs
       </Link>
       <Link
-        href="https://www.reservix.net/karriere/"
+        // @ts-ignore
+        to="https://www.reservix.net/karriere/"
         className="text-xs underline-offset-4 hover:underline"
       >
         Reservix GmbH
