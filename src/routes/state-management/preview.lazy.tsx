849406961a623b59ec99e574f076f831c7632567
Sebastian Sebald <sebastian.sebald@gmail.com>
2024-07-02 11:42:03 +0200

    storyline

--- src/routes/state-management/preview.lazy.tsx
+++ src/routes/state-management/preview.lazy.tsx
@@ -2,12 +2,15 @@ import { createLazyFileRoute } from '@tanstack/react-router';
 import theme from '@marigold/theme-core';
 import { MarigoldProvider } from '@marigold/components';
 
+import { Block } from '@/components/Container';
 import ServerStateExample from './_components/ServerStateExample';
 
 export const Route = createLazyFileRoute('/state-management/preview')({
   component: () => (
-    <MarigoldProvider theme={theme}>
-      <ServerStateExample />
-    </MarigoldProvider>
+    <Block data-theme="core">
+      <MarigoldProvider theme={theme}>
+        <ServerStateExample />
+      </MarigoldProvider>
+    </Block>
   ),
 });
