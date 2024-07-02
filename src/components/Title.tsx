063acb677ba6a0d8ec721b3cf377a8932f56ae91
Sebastian Sebald <sebastian.sebald@gmail.com>
2024-07-02 12:48:43 +0200

    refa: adjust background of tutorials

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -15,7 +15,7 @@ export const Title = ({ title, caption, image }: TitleProps) => (
     className={[
       'relative grid place-items-center',
       'bg-[linear-gradient(to_bottom,_var(--tw-gradient-stops)),var(--bg-image)] bg-repeat [background-size:auto,var(--bg-size)]',
-      'from-violet-700/15 via-secondary-50 via-70% to-secondary-50',
+      'from-primary-800/25 via-secondary-50 via-80% to-secondary-50',
     ].join(' ')}
     style={
       {
@@ -26,7 +26,7 @@ export const Title = ({ title, caption, image }: TitleProps) => (
   >
     <div className="absolute inset-x-0 top-0 h-8 bg-gradient-to-b from-neutral-700/50 via-neutral-700/10 via-20% to-transparent" />
 
-    <div className="px-20 pb-32 pt-52 text-center [background-image:radial-gradient(ellipse_at_bottom,rgba(248,250,252,1)_0%,rgba(248,250,252,1)_40%,transparent_70%)]">
+    <div className="px-20 pb-32 pt-52 text-center [background-image:radial-gradient(ellipse_at_center_90%,rgba(248,250,252,1)_0%,rgba(248,250,252,1)_40%,transparent_60%)]">
       <h1 className="p-0 text-secondary-950">{title}</h1>
       <div className="-mt-8 text-lg text-secondary-950/60">{caption}</div>
     </div>
