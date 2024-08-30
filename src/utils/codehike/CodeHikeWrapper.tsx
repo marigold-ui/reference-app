d00bac87d8ac7320eca0486af9616aa2d92795d5
aromko <aromkoapps@gmail.com>
2024-08-30 13:57:36 +0200

    DST-538: add scrollycoding feature

--- src/utils/codehike/CodeHikeWrapper.tsx
+++ src/utils/codehike/CodeHikeWrapper.tsx
@@ -1,9 +1,11 @@
 import { type HighlightedCode, Pre } from 'codehike/code';
 import { mark } from '@/utils/codehike/mark';
 import { focus } from '@/utils/codehike/focus';
+import { tokenTransitions } from '@/utils/codehike/scrollycoding/token-transition';
 
 function CodeHikeWrapper({ codeblock }: { codeblock: HighlightedCode }) {
-  return <Pre code={codeblock} handlers={[mark, focus]} />;
+  console.log(JSON.stringify(codeblock));
+  return <Pre code={codeblock} handlers={[mark, focus, tokenTransitions]} />;
 }
 
 export default CodeHikeWrapper;
