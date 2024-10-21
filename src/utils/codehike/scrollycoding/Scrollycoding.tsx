15794507ac0dcb5a47bf6ed19743bc7ca3c7823e
aromko <aromkoapps@gmail.com>
2024-10-21 15:09:38 +0200

    fix eslint

--- src/utils/codehike/scrollycoding/Scrollycoding.tsx
+++ src/utils/codehike/scrollycoding/Scrollycoding.tsx
@@ -15,13 +15,13 @@ export function Scrollycoding(props: unknown) {
   const { steps } = parseProps(props, Schema);
   return (
     <SelectionProvider className="flex gap-4">
-      <div className="flex-1 mt-32 mb-[90vh] ml-2 prose prose-invert">
+      <div className="prose prose-invert mb-[90vh] ml-2 mt-32 flex-1">
         {steps.map((step, i) => (
           <Selectable
             key={i}
             index={i}
             selectOn={['click', 'scroll']}
-            className="border-l-4 border-zinc-700 data-[selected=true]:border-blue-400 px-5 py-2 mb-24 rounded bg-zinc-900"
+            className="mb-24 rounded border-l-4 border-zinc-700 bg-zinc-900 px-5 py-2 data-[selected=true]:border-blue-400"
           >
             <h2 className="mt-4 text-xl">{step.title}</h2>
             <div>{step.children}</div>
@@ -29,7 +29,7 @@ export function Scrollycoding(props: unknown) {
         ))}
       </div>
       <div className="w-[40vw] max-w-xl bg-zinc-900">
-        <div className="top-16 sticky overflow-auto">
+        <div className="sticky top-16 overflow-auto">
           <Selection
             from={steps.map(step => {
               return <CodeHikeWrapper codeblock={step.code} />;
